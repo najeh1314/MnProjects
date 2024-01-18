42da0461ad6407859ecee487da5c7398cc08aba5
Najeh <mn.najeh1314@gmail.com>
2024-01-18 09:58:18 +0100

    return back to last hache

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const dateEl = document.querySelector("#date");
 const currentDate = new Date();
-dateEl.textContent = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+dateEl.textContent = currentDate.getDate();
+dateEl.textContent += "/"+currentDate.getMonth()+1;
+dateEl.textContent += "/"+currentDate.getFullYear();
 
 const coffeeMenuEL = document.querySelector("#coffee-menu");
+coffeeMenuEL.innerHTML += `
+<article class="item">
+    <p class="flavor">French Vanilla</p><p class="price">3.00</p>
+</article>
+<article class="item">
+    <p class="flavor">Caramel Macchiato</p><p class="price">3.75</p>
+</article>
+<article class="item">
+    <p class="flavor">Pumpkin Spice</p><p class="price">3.50</p>
+</article>
+<article class="item">
+    <p class="flavor">Hazelnut</p><p class="price">4.00</p>
+</article>
+<article class="item">
+    <p class="flavor">Mocha</p><p class="price">4.50</p>
+</article>
+`;
 const dessertMenuEL = document.querySelector("#dessert-menu");
-
-// Coffee menu items
-const coffeeMenuItems = [
-  { flavor: "French Vanilla", price: 3.00 },
-  { flavor: "Caramel Macchiato", price: 3.75 },
-  { flavor: "Pumpkin Spice", price: 3.50 },
-  { flavor: "Hazelnut", price: 4.00 },
-  { flavor: "Mocha", price: 4.50 }
-];
-
-// Dessert menu items
-const dessertMenuItems = [
-  { dessert: "Donut", price: 1.50 },
-  { dessert: "Cherry Pie", price: 2.75 },
-  { dessert: "Cheesecake", price: 3.00 },
-  { dessert: "Cinnamon Roll", price: 2.50 }
-];
-
-// Function to create menu items dynamically
-function createMenuItems(menuEl, items) {
-  items.forEach(item => {
-    menuEl.innerHTML += `
-      <article class="item">
-        <p class="${item.flavor ? 'flavor' : 'dessert'}">${item.flavor || item.dessert}</p>
-        <p class="price">${item.price.toFixed(2)}</p>
-      </article>
-    `;
-  });
-}
-
-// Populate coffee and dessert menus
-createMenuItems(coffeeMenuEL, coffeeMenuItems);
-createMenuItems(dessertMenuEL, dessertMenuItems);
+dessertMenuEL.innerHTML += `
+<article class="item">
+            <p class="dessert">Donut</p><p class="price">1.50</p>
+          </article>
+          <article class="item">
+            <p class="dessert">Cherry Pie</p><p class="price">2.75</p>
+          </article>
+          <article class="item">
+            <p class="dessert">Cheesecake</p><p class="price">3.00</p>
+          </article>
+          <article class="item">
+            <p class="dessert">Cinnamon Roll</p><p class="price">2.50</p>
+          </article>
+`;
